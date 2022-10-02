caa38d39765702f280e399ab274866bf837ddc92
Dan C <djc5627@gmail.com>
2022-10-02 03:06:27 -0400

    Add select AI picture time.

--- Assets/Scripts/Clickable.cs
+++ Assets/Scripts/Clickable.cs
@@ -8,10 +8,24 @@ namespace BML.Scripts {
         [SerializeField] private bool _stop;
         [SerializeField] private UnityEvent _onClick;
 
+        public delegate void OnClick();
+        public event OnClick _OnClick;
+
         public bool ShouldStop { get { return _stop; } }
 
         public void EmitClick() {
             _onClick.Invoke();
+            _OnClick?.Invoke();
+        }
+
+        public void SubscribeOnClick(OnClick callback)
+        {
+            _OnClick += callback;
+        }
+        
+        public void UnSubscribeOnClick(OnClick callback)
+        {
+            _OnClick -= callback;
         }
     }
 }
---
+++ Assets/Scripts/Minigames/AiMinigamePreset.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+namespace BML.Scripts
+{
+    [CreateAssetMenu(fileName = "AiMinigamePreset/", menuName = "BML/Minigame/AiMinigamePreset", order = 0)]
+    public class AiMinigamePreset : ScriptableObject
+    {
+        public string Prompt = "Pick the correct picture";
+        public Sprite CorrectImage;
+        public List<Sprite> WrongImages;
+    }
+}
