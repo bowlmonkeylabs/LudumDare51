720ce9da5415a2385f1076dd8e2ebb35d6579253
Dan C <djc5627@gmail.com>
2022-10-02 15:57:40 -0400

    Add brush teeth minigame.

---
+++ Assets/Scripts/Minigames/BrushTeethMinigameController.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using BML.Scripts.ScriptableObjects;
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+namespace BML.Scripts
+{
+    public class BrushTeethMinigameController : MonoBehaviour
+    {
+        [SerializeField] private MinigameTask _task;
+        [SerializeField] private GameObject _plaquePrefab;
+        [SerializeField] private int _plaqueCount;
+        [SerializeField] private List<Transform> _plaqueSpawnPoints = new List<Transform>();
+        
+        private List<Plaque> plaques = new List<Plaque>();
+
+        private void Start()
+        {
+            //Instantiate plants on spawn points with removal
+            List<Transform> spawnPointTemp = _plaqueSpawnPoints;
+            for (int i = 0; i < _plaqueCount; i++)
+            {
+                int randIndex = Random.Range(0, spawnPointTemp.Count);
+                GameObject plant = Instantiate(_plaquePrefab, spawnPointTemp[randIndex]);
+                plaques.Add(plant.GetComponent<Plaque>());
+                spawnPointTemp.Remove(spawnPointTemp[randIndex]);
+            }
+            
+            foreach (var plant in plaques)
+            {
+                plant.onCleaned += CheckMinigameComplete;
+            }
+        }
+
+        private void OnDisable()
+        {
+            foreach (var plant in plaques)
+            {
+                plant.onCleaned -= CheckMinigameComplete;
+            }
+        }
+
+        private void CheckMinigameComplete()
+        {
+            foreach (var plant in plaques)
+            {
+                if (!plant.IsCleaned)
+                    return;
+            }
+            
+            _task.InvokeOnSuccess();
+        }
+    }
+}
---
+++ Assets/Scripts/Minigames/Plaque.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+using Random = UnityEngine.Random;
+
+namespace BML.Scripts
+{
+    public class Plaque : MonoBehaviour
+    {
+        [SerializeField] private List<GameObject> _plaqueSpots = new List<GameObject>();
+        
+        public delegate void OnCleaned();
+        public event OnCleaned onCleaned;
+
+        public UnityEvent _OnCleaned;
+        public bool IsCleaned => isCleaned;
+
+        private bool isCleaned;
+        
+        private List<GameObject> _plaqueSpotsModify = new List<GameObject>();
+
+        private void Start()
+        {
+            _plaqueSpotsModify = _plaqueSpots;
+        }
+
+        public void DoClean()
+        {
+            if (_plaqueSpotsModify.Count == 0)
+            {
+                SetCleaned();
+                return;
+            }
+                
+
+            int randomIndex = Random.Range(0, _plaqueSpotsModify.Count);
+            _plaqueSpotsModify[randomIndex].SetActive(false);
+            _plaqueSpotsModify.RemoveAt(randomIndex);
+        }
+
+        public void SetCleaned()
+        {
+            isCleaned = true;
+            _OnCleaned.Invoke();
+            onCleaned?.Invoke();
+        }
+    }
+}
