c3e563498006ba0ab46f3101e9f0f75302a6e102
Dan C <djc5627@gmail.com>
2022-10-02 15:11:15 -0400

    Add gardening minigame.

---
+++ Assets/Scripts/Minigames/GardeningMinigameController.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using BML.Scripts.ScriptableObjects;
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+namespace BML.Scripts
+{
+    public class GardeningMinigameController : MonoBehaviour
+    {
+        [SerializeField] private MinigameTask _task;
+        [SerializeField] private GameObject _plantPrefab;
+        [SerializeField] private int _plantCount;
+        [SerializeField] private List<Transform> _plantSpawnPoints = new List<Transform>();
+        
+        private List<Plant> plants = new List<Plant>();
+
+        private void Start()
+        {
+            //Instantiate plants on spawn points with removal
+            List<Transform> spawnPointTemp = _plantSpawnPoints;
+            for (int i = 0; i < _plantCount; i++)
+            {
+                int randIndex = Random.Range(0, spawnPointTemp.Count);
+                GameObject plant = Instantiate(_plantPrefab, spawnPointTemp[randIndex]);
+                plants.Add(plant.GetComponent<Plant>());
+                spawnPointTemp.Remove(spawnPointTemp[randIndex]);
+            }
+            
+            foreach (var plant in plants)
+            {
+                plant.onWatered += CheckMinigameComplete;
+            }
+        }
+
+        private void OnDisable()
+        {
+            foreach (var plant in plants)
+            {
+                plant.onWatered -= CheckMinigameComplete;
+            }
+        }
+
+        private void CheckMinigameComplete()
+        {
+            foreach (var plant in plants)
+            {
+                Debug.Log(plant.IsWatered);
+                if (!plant.IsWatered)
+                    return;
+            }
+            
+            _task.InvokeOnSuccess();
+        }
+    }
+}
---
+++ Assets/Scripts/Minigames/Plant.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+
+namespace BML.Scripts
+{
+    public class Plant : MonoBehaviour
+    {
+        public delegate void OnWatered();
+        public event OnWatered onWatered;
+
+        public UnityEvent _OnWatered;
+        public bool IsWatered => isWatered;
+
+        private bool isWatered;
+
+        public void SetWatered()
+        {
+            isWatered = true;
+            _OnWatered.Invoke();
+            onWatered?.Invoke();
+        }
+    }
+}
